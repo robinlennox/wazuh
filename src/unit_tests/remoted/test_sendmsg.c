543229d8e8ce19848a2d8ee5fe607acc84202044
Francisco Tuduri <francisco.tuduri@wazuh.com>
2022-06-23 15:14:08 -0300

    Add UT

--- src/unit_tests/remoted/test_sendmsg.c
+++ src/unit_tests/remoted/test_sendmsg.c
@@ -39,14 +39,16 @@ static int group_teardown(void ** state) {
     return 0;
 }
 
-static int test_setup_tcp(void ** state) {
+
+static int test_setup_keys(void ** state) {
     test_mode = 1;
 
     keyentry *node_key = NULL;
     os_calloc(1, sizeof(keyentry), node_key);
-    node_key->net_protocol = REMOTED_NET_PROTOCOL_TCP;
     node_key->rcvd = 10;
     node_key->sock = 15;
+    os_strdup("001",node_key->id);
+
     os_calloc(2, sizeof(keyentry*), keys.keyentries);
     keys.keyentries[0] = node_key;
     keys.keyentries[1] = NULL;
@@ -54,13 +56,38 @@ static int test_setup_tcp(void ** state) {
     return 0;
 }
 
-static int test_teardown_tcp(void ** state) {
+static int test_teardown_keys(void **state){
     test_mode = 0;
 
+    os_free(keys.keyentries[0]->id);
     os_free(keys.keyentries[0]);
     os_free(keys.keyentries);
 
     return 0;
+}
+
+static int test_setup_tcp(void ** state) {
+
+    test_setup_keys(state);
+    keys.keyentries[0]->net_protocol = REMOTED_NET_PROTOCOL_TCP;
+
+    return 0;
+}
+
+static int test_teardown_tcp(void ** state) {
+    return test_teardown_keys(state);
+}
+
+static int test_setup_udp(void ** state) {
+
+    test_setup_keys(state);
+    keys.keyentries[0]->net_protocol = REMOTED_NET_PROTOCOL_UDP;
+
+    return 0;
+}
+
+static int test_teardown_udp(void ** state) {
+    return test_teardown_keys(state);
 }
 
 /* Tests */
@@ -164,10 +191,58 @@ void test_send_msg_tcp_err(void ** state) {
     assert_int_equal(remoted_state.queued_msgs, 0);
 }
 
+void test_send_msg_udp_ok(void ** state) {
+    (void) state;
+
+    char *agent_id = "001";
+    char *msg = "abcdefghijk";
+    ssize_t msg_length = strlen(msg);
+    int key = 0;
+
+    char *crypto_msg = "!@#123abc";
+    ssize_t crypto_size = strlen(crypto_msg);
+    
+    logr.global.agents_disconnection_time = 0;
+    remoted_state.queued_msgs = 0;
+
+    expect_function_call(__wrap_pthread_rwlock_rdlock);
+
+    expect_string(__wrap_OS_IsAllowedID, id, agent_id);
+    will_return(__wrap_OS_IsAllowedID, key);
+
+    expect_function_call(__wrap_pthread_mutex_lock);
+
+    will_return(__wrap_time, (time_t)0);
+
+    expect_function_call(__wrap_pthread_mutex_unlock);
+
+    expect_string(__wrap_CreateSecMSG, msg, msg);
+    expect_value(__wrap_CreateSecMSG, msg_length, msg_length);
+    expect_value(__wrap_CreateSecMSG, id, key);
+    will_return(__wrap_CreateSecMSG, crypto_size);
+    will_return(__wrap_CreateSecMSG, crypto_msg);
+
+    expect_function_call(__wrap_pthread_mutex_lock);
+
+    will_return(__wrap_sendto, crypto_size);
+
+    expect_value(__wrap_rem_add_send, bytes, crypto_size);
+    expect_function_call(__wrap_rem_add_send);
+
+    expect_function_call(__wrap_pthread_mutex_unlock);
+    expect_function_call(__wrap_pthread_rwlock_unlock);
+
+    int ret = send_msg(agent_id, msg, msg_length);
+
+    assert_int_equal(ret, 0);
+    assert_int_equal(remoted_state.queued_msgs, 0);
+}
+
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test_setup_teardown(test_send_msg_tcp_ok, test_setup_tcp, test_teardown_tcp),
         cmocka_unit_test_setup_teardown(test_send_msg_tcp_err, test_setup_tcp, test_teardown_tcp),
+        cmocka_unit_test_setup_teardown(test_send_msg_udp_ok, test_setup_udp, test_teardown_udp),
     };
     return cmocka_run_group_tests(tests, group_setup, group_teardown);
 }
